57530c946b262ce1c192ed7a4e034c13a899d297
Amarilio de Oliveira <amarilioalencar@gmail.com>
2023-02-08 14:37:28 -0300

    Theme color changes, add Favicon and some minor code fix.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      'lavander': '#eeefff',
+      'corn-flower': '#7597de',
+      'blue-violet': '#695bae',
+      'russian-violet': '#2b1055',
+      'dark-purple': '#1c0522',
+    },
     extend: {},
   },
   plugins: [],
