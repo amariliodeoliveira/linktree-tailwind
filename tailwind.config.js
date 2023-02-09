15d4f2454ef5d8138c0ded1afb3b75ac845eac00
Amarilio de Oliveira <amarilioalencar@gmail.com>
2023-02-09 18:04:39 -0300

    Add background animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,29 @@ module.exports = {
 
   theme: {
     colors: {
-      lavander: "#eeefff",
-      "corn-flower": "#7597de",
-      "blue-violet": "#695bae",
-      "russian-violet": "#2b1055",
-      "dark-purple": "#1c0522",
+      'lavander': '#eeefff',
+      'corn-flower': '#7597de',
+      'blue-violet': '#695bae',
+      'russian-violet': '#2b1055',
+      'dark-purple': '#1c0522',
     },
+
     extend: {
+      backgroundImage: {
+        'bg': 'linear-gradient(-60deg, #1c0522 50%, #2b1055 50%)',
+      },
+
       keyframes: {
-        slide: {
-          "0%": { transform: "translateX(-25%)" },
-          "100%": { transform: "translateX(25%)" },
+        'slide': {
+          '0%': { transform: 'translateX(-25%)' },
+          '100%': { transform: 'translateX(25%)' },
         },
+      },
+
+      animation: {
+        'slide-normal': 'slide 3s ease-in-out infinite alternate',
+        'slide-fast': 'slide 4s ease-in-out infinite alternate-reverse',
+        'slide-fastest': 'slide 5s ease-in-out infinite alternate',
       },
     },
   },
